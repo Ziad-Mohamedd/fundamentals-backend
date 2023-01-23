2a0c86898e47984e5ea6471efb63083733311979
Ziad-Mohamedd <zeyadmohamed613@gmail.com>
2023-01-23 09:47:09 +0200

    pub/sub demo

---
+++ pub-sub/publisher.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+
+const msg = {number: process.argv[2]}
+connect();
+
+async function connect() {
+    try {
+        const amqpServer = "amqps://sivnurje:tgRPmnusEROVhpamhgHKQ80fZf72KVp7@rattlesnake.rmq.cloudamqp.com/sivnurje"
+        const connection = await amqp.connect(amqpServer)
+        const channel = await connection.createChannel();
+        await channel.assertQueue("jobs");
+        await channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+        console.log(`Job sent successfully ${msg.number}`);
+        await channel.close();
+        await connection.close();
+    }
+    catch (ex) {
+        console.error(ex);
+    }
+}
